fb782b1baf16dc554d9f954aa4c96e5e928c7abb
Ben-bit-code208 <b64964357@gmail.com>
2025-11-09 16:25:53 +0100

    deleted:    .vscode/c_cpp_properties.json 	deleted:    .vscode/launch.json 	deleted:    .vscode/settings.json 	deleted:    Archiv.html 	deleted:    DATA/Cloud/hi.c 	deleted:    DATA/Cloud/mcdateien/TEST.txt 	deleted:    DATA/Cloud/start.v 	deleted:    DATA/First Run.txt 	deleted:    DATA/SchulSpiel.exe 	deleted:    DATA/Spiele/Mr. X.exe 	deleted:    DATA/Spiele/__pycache__/gettinghacked.cpython-314.pyc 	deleted:    DATA/Spiele/__pycache__/irgendwas.cpython-311.pyc 	deleted:    DATA/Spiele/__pycache__/irgendwas.cpython-314.pyc 	deleted:    DATA/Spiele/gettinghacked.py     deleted:    DATA/Spiele/icon.ico 	deleted:    DATA/Spiele/irgendwas.py 	deleted:    DATA/Spiele/longpathsenable.reg 	deleted:    DATA/Spiele/matrix.py 	deleted:    DATA/Spiele/phonering.py 	deleted:    DATA/Spiele/test.py 	deleted:    DATA/Spiele/test.v 	deleted:    De.html 	deleted:    En.html 	deleted:    LICENSE 	deleted:    github.html 	deleted:    index.html
    	.gitattributes
    	.vscode/
    	Archiv.html
    	DATA/
    	De.html
    	En.html
    	LICENSE
    	github.html
    	index.html
    	tmp_check_url.py
    	tmp_fetch_blob.py
    	tmp_test_download.py

--- DATA/Spiele/test.py
+++
@@ -1,7 +0,0 @@
-import os
-if os.path.exists("/mnt"):
- print("this is linux or android")
-elif os.path.exists("C:\window") or ("C:\Window"):
- print("this is windows")
-else:
- print ("the os isn't recognized")
--- DATA/Spiele/test.v
+++
@@ -1,9 +0,0 @@
-module main
-import os
-
-fn main() {
-    name := os.input('Wie heißt du? ')
-    println('Hey $name, willkommen in V!')
-    println('V ist eine tolle Programmiersprache.')
-    println(":)")
-}
